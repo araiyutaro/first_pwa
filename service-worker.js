80c765ad968e8372b0cdc5959ceec0a36e1917c0
arai <arai@slow-walkers.jp>
2019-04-16 11:05:47 +0900

    PWAに必要な最低限の要素を追加

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+var cacheName = 'arai_test';
+var fileToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/styles/inline.css'
+];
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker_arai] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker_arai] Caching app shell');
+            return cache.addAll(fileToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker_arai] Activate');
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker_arai] Fetch');
+});
